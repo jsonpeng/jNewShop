18fa992711254bbc9f7188b48fb5903e6cc98eb6
jsonpeng <1127915086@qq.com>
2019-03-19 22:39:24 +0800

    ss

--- resources/views/front/default/check.blade.php
+++ resources/views/front/default/check.blade.php
@@ -66,7 +66,7 @@
 
     <?php $leaderName = $user->LeaderNameFront; ?>
     @if($leaderName != '无')
-    <div style="margin-left: 15px;"><img src="{{ asset('images/social/index.png') }}" alt="" class="" style="max-width: 20px;height: auto;">{{ $leaderName }}的店铺</div>
+    <div style="margin-top:15px;margin-left: 15px;"><img src="{{ asset('images/social/index.png') }}" alt="" class="" style="max-width: 20px;height: auto;">{{ $leaderName }}的店铺</div>
     @endif
 
     <div class="check-products">
